c01ee95d4c68f46a76bfe7e336b4404938a24822
Youcef <youcefboutiche@hotmail.fr>
2024-12-26 14:22:47 +0100

    ajout de api platforme dans mes entites

--- repo_symfony/src/Entity/Matiere.php
+++ repo_symfony/src/Entity/Matiere.php
@@ -6,8 +6,10 @@ use App\Repository\MatiereRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: MatiereRepository::class)]
+#[ApiResource]
 class Matiere
 {
     #[ORM\Id]
--- repo_symfony/src/Entity/User.php
+++ repo_symfony/src/Entity/User.php
@@ -8,10 +8,12 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Entity\Enseignant; // Importing Enseignant entity
 use App\Entity\Promotion; // Importing Promotion entity
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[ApiResource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
